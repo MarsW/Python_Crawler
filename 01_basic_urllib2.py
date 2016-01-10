933fc721068b6b3912105be0f974688970f338c9
MarsW <ymjh11436@gmail.com>
2016-01-10 11:29:11 +0800

    01_basic code

---
+++ 01_basic_urllib2.py
@@ -0,0 +1,10 @@
+import urllib2
+
+request = urllib2.Request("http://blog.marsw.tw")
+response = urllib2.urlopen(request)
+html = response.read()
+print html
+
+fileout = file("01_blog.html","w")
+fileout.write(html)
+fileout.close()
